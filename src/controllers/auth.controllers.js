f2af554aa5c28ca51afe006a9a15317408a61d82
Ramiro <ramiroroman306@gmail.com>
2025-09-26 15:39:15 -0300

    Se creo el registro de usuarios, el login y el logout.

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,29 +1,86 @@
+import { comparePassword, hashPassword } from "../helpers/bcrypt.helper.js";
+import { generateToken } from "../helpers/jwt.helper.js";
 import { UserModel } from "../models/user.model.js";
-import {matchedData} from "express-validator";
-// Register de usuario
+import { matchedData } from "express-validator";
+
+// Registro de usuario
 export const register = async (req, res) => {
-    try {
-        const data = matchedData(req);
+  try {
+    // Extraer los datos que pasaron la validacion
+    const data = matchedData(req);
 
-        const user = await UserModel.create(data)
+    // Hashear la contraseña
+    const hashedPassword = await hashPassword(data.password);
 
-        res.status(201).json({ok: true, msg: 'Usuario creado correctamente', user})
-
-        
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({
-        ok: false,
-        message: "Error interno del servidor",err
+    // Creacion del usuario con los datos validados
+    const user = await UserModel.create({
+      username: data.username,
+      email: data.email,
+      password: hashedPassword, // Password hasheada
+      role: data.role,
+      profile: data.profile,
     });
-    }
-}
+
+    // Devuelve el usuario creado
+    res
+      .status(201)
+      .json({ ok: true, msg: "Usuario creado correctamente", user });
+  } catch (err) {
+    res.status(500).json({
+      ok: false,
+      message: "Error interno del servidor",
+      err,
+    });
+  }
+};
 
 // Login de usuario
 export const login = async (req, res) => {
-    try {
-        
-    } catch (err) {
-        
-    }
-}
+  const { username, password } = req.body;
+  try {
+    // Buscar el usuario por su username
+    const user = await UserModel.findOne({ username });
+    if (!user)
+      return res.status(404).json({ ok: false, msg: "Usuario no encontrado" });
+
+    // Comparar la contraseña recibida con la almacenada
+    const isMatch = await comparePassword(password, user.password);
+
+    // Si no coinciden las contraseñas
+    if (!isMatch)
+      return res.status(401).json({ ok: false, msg: "Contraseña incorrecta" });
+
+    // Generar el token con JWT
+    const token = generateToken(user);
+
+    // Enviar el token en una cookie
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60,
+    }); // 1 hora
+
+    res.status(200).json({ ok: true, message: "Login exitoso", token });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ ok: false, msg: "Error interno del servidor", err });
+  }
+};
+
+// Logout de usuario
+export const logout = async (req, res) => {
+  try {
+    // Eliminar la cookie del token
+    res.clearCookie("token");
+
+    res.status(200).json({
+      ok: true,
+      message: "Logout exitoso",
+    });
+  } catch (err) {
+    res.status(500).json({
+      ok: false,
+      message: "Error interno del servidor",
+      err,
+    });
+  }
+};
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
-import { register } from "../controllers/auth.controllers.js";
+import { login, logout, register } from "../controllers/auth.controllers.js";
 import { creatUserValidations } from "../middlewares/validations/user.validations.js";
+import { applyValidations } from "../middlewares/validator.js";
 
 export const authRouter = Router();
-authRouter.post("/auth/register", creatUserValidations, register);
+authRouter.post("/auth/register", creatUserValidations, applyValidations, register);
+authRouter.post("/auth/login", login);
+authRouter.post("/auth/logout", logout);
